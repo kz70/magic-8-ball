c66dae67cb774545b8621c39e92353cd27aaeb41
Kailin Zhang <kailin11511@gmail.com>
2021-07-27 09:02:09 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Magic 8 Ball 🎱
+
+## Overview
+- Magic 8 ball app that will give you the answers to any question, created in Flutter
+- Project created during the London App Brewery Flutter Course
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purple,
+      appBar: AppBar(
+          backgroundColor: Colors.purple[900], title: Text('Ask Me Anything')),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
